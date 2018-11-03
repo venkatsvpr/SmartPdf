6a6b904c32f3899f0a0e79929e5289e82076ad34
Shubham4792 <shubhampandey4792@gmail.com>
2018-11-03 19:13:09 -0400

    UI completed

--- gui/pdfsite/SmartPdf/models.py
+++ gui/pdfsite/SmartPdf/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class File(models.Model):
 
     name = models.CharField(max_length=255)
-    pages = models.CharField(max_length=4)
+    pages = models.CharField(max_length=15)
 
     class Meta:
         db_table = 'file'
--- gui/pdfsite/SmartPdf/views.py
+++ gui/pdfsite/SmartPdf/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render
 from .forms import FileFormset
 from .models import File
+import sys
 
+#sys.path.append(0,")
 # Create your views here.
 
 def index(request):
@@ -10,16 +12,19 @@ def index(request):
     if request.method == 'GET':
         formset = FileFormset(request.GET or None)
     elif request.method == 'POST':
+        for x in File.objects.all():
+            x.delete()
+
         formset = FileFormset(request.POST)
         if formset.is_valid():
             for form in formset:
                 # extract name from each form and save
                 name = form.cleaned_data.get('name')
+                pagenos = form.cleaned_data.get('pages')
                 # save file instance
-                if name:
-                    File(name=name).save()
-            # once all files are saved, redirect to file list view
-            return redirect('file_list')
+                if name and pagenos:
+                    File(name=name,pages = pagenos).save()
+                
     return render(request, template_name, {
         'formset': formset,
         'heading': heading_message,
