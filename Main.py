c730cf074fb2df08c0ca627644c582239a9ea62d
Venkata Krishnan Anantha Raman <venkatakrishnansvpr@gmail.com>
2018-11-03 17:32:31 -0400

    Adding Watermark

--- Main.py
+++ Main.py
@@ -19,6 +19,44 @@ def expandPages (strPageList, maxPageCount):
            page_numbers.append(int(p))
    return page_numbers
 
+def WaterMark (inputPdfPath, waterMarkPath, pageList, outputPdfPath):
+    """
+    Add WaterMark to the inputFile for the pages in the pageList.
+    Use the watermark from the waterMarkPath, the watermark pdf should have only one page
+    Store the output on the outputPdfPath
+    
+    :param inputPdfPath: List of paths of input pdf files
+    :param waterMarkPath: String to the watermark path
+    :param pageList:  List of page numbers where we need the watermark
+    :param outputPdfPath:
+    :return:
+    """
+    inputFile = open(inputPdfPath, "rb")
+    inFileReader = PyPDF2.PdfFileReader(inputFile)
+
+    waterMarkFile = open(waterMarkPath, "rb")
+    wmFileReader = PyPDF2.PdfFileReader(waterMarkFile)
+
+    pdfWriter = PyPDF2.PdfFileWriter()
+    if (pageList == None):
+        pageSet = set(range(0,inFileReader.numPages))
+    else:
+        pageSet = set(pageList)
+
+    for page in range(inFileReader.numPages):
+        pageObject = inFileReader.getPage(page)
+        if (page in PageSet):
+            pageObject.mergePage(wmFileReader.getPage(0))
+        pdfWriter.addPage(pageObject)
+
+    inputFile.close()
+    waterMarkFile.close()
+
+    outputFile = open(outputPdfPath, "wb")
+    pdfWriter.write(outputFile)
+    outputFile.close()
+    return
+
 def MergePdf (pathToInputs, pageList, orientationDict, pathToOutputPdf):
     """
     TODO:
