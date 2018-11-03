8c19a90c013baa342ae1e8c7224913b9aded747b
Venkata Krishnan Anantha Raman <venkatakrishnansvpr@gmail.com>
2018-11-03 16:34:35 -0400

    updating MergePdf

--- Main.py
+++ Main.py
@@ -11,6 +11,8 @@ def MergePdf (pathToInputs, pageList, orientationDict, pathToOutputPdf):
     TODO:
     Orientation
     Testing the method
+
+    Open the input file, pick the pages passed, do the orientation and push it to output.
     :param pathToInputs:
     :param pageList:
     :param orientationDict:
@@ -24,13 +26,16 @@ def MergePdf (pathToInputs, pageList, orientationDict, pathToOutputPdf):
             pdf = PyPDF2.PdfFileReader(inputFileObject)
             for page in pages:
                 if (0 <= page < pdf.numPags):
-                    pdfWriter.addPage(pdf.getPage(page))
+                    pageObject = pdf.getPage(page)
+                    #pageObject.rotateClockwise(90)
+                    pdfWriter.addPage(pageObject)
             inputFileObject.close()
         except:
-            continue;
+            continue
     try:
         outFile = open(pathToOutputPdf,"wb")
         pdfWriter.write(outFile)
         outFile.close()
     except:
         continue
+
