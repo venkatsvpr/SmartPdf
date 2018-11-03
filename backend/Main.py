eb2063c573018451c00d8d9895a0c4c6e0597bbd
Venkata Krishnan Anantha Raman <venkatakrishnansvpr@gmail.com>
2018-11-03 18:48:29 -0400

    More Code

--- backend/Main.py
+++ backend/Main.py
@@ -1,24 +1,5 @@
 from Globals import *
 
-def expandPages (strPageList, maxPageCount):
-   """
-   :param strPageList:
-   :return: list of page numbers
-   """
-   pages = strPageList.split(',')
-   page_numbers = []
-   for p in pages:
-       if '-' in p:
-           first_last = p.split('-')
-           start = int(first_last[0])
-           while(start <= int(first_last[1])):
-               if start < maxPageCount:
-                   page_numbers.append(start)
-               start += 1
-       else:
-           page_numbers.append(int(p))
-   return page_numbers
-
 def WaterMark (inputPdfPath, waterMarkPath, pageList, outputPdfPath):
     """
     Add WaterMark to the inputFile for the pages in the pageList.
@@ -45,34 +26,52 @@ def WaterMark (inputPdfPath, waterMarkPath, pageList, outputPdfPath):
 
     for page in range(inFileReader.numPages):
         pageObject = inFileReader.getPage(page)
-        if (page in PageSet):
+        if (page in pageSet):
             pageObject.mergePage(wmFileReader.getPage(0))
         pdfWriter.addPage(pageObject)
-
-    inputFile.close()
-    waterMarkFile.close()
-
     outputFile = open(outputPdfPath, "wb")
     pdfWriter.write(outputFile)
+
     outputFile.close()
+    inputFile.close()
+    waterMarkFile.close()
     return
+
+def expandPages (strPageList, maxPageCount):
+   """
+   :param strPageList:
+   :return: list of page numbers
+   Input "1,2-4,10,4"
+   Output  [1,2,3,4,10,4]
+   """
+   pages = strPageList.split(',')
+   page_numbers = []
+   for p in pages:
+       if '-' in p:
+           first_last = p.split('-')
+           start = int(first_last[0])
+           while(start <= int(first_last[1])):
+               if start < maxPageCount:
+                   page_numbers.append(start)
+               start += 1
+       else:
+           page_numbers.append(int(p))
+   return page_numbers
 
 def MergePdf (pathToInputs, pageList, orientationDict, pathToOutputPdf):
     """
     TODO:
-    Orientation
-    Testing the method
-
+    Orientation, Testing the method
     Open the input file, pick the pages passed, do the orientation and push it to output.
-    :param pathToInputs: List of paths
-    :param pageList:
-    :param orientationDict:
-    :param pathToOutputPdf:
+    :param pathToInputs: List of path to input files
+    :param pageList: use the output of expandPages
+    :param orientationDict: Ignore
+    :param pathToOutputPdf: path to outputfile
     :return:
     """
     pdfWriter = PyPDF2.PdfFileWriter()
     outFile = open(pathToOutputPdf,"wb+")
-    for inputFilePath,pages,orient in zip(pathToInputs, pageList, orientationDict):
+    for inputFilePath,pages in zip(pathToInputs, pageList):
         inputFileObject = open(inputFilePath, "rb")
         pdf = PyPDF2.PdfFileReader(inputFileObject)
         for page in pages:
@@ -84,7 +83,28 @@ def MergePdf (pathToInputs, pageList, orientationDict, pathToOutputPdf):
         inputFileObject.close()
     outFile.close()
 
+def getMaxPageCount (inputFilePath):
+    """
+    Returns the Max Page Count of an input file
+    :param inputFilePath:
+    :return:
+    """
+    inputFile = open(inputFilePath, "rb")
+    inFileReader = PyPDF2.PdfFileReader(inputFile)
+    numberOfPages = inFileReader.numPages
+    inputFile.close()
+    return numberOfPages
 
+def SplitPagesFromPdfFile (inputFilePath, strSplitRange, outputFilePath):
+    """
+    Split a part of the Input file and creates another PDF File
+    :param inputFilePath: Input File path
+    :param strSplitRange: Range of the split in string format
+    :param outputFilePath: Output File Path
+    :return: Nothing
+    """
+    pageList = expandPages (strSplitRange, getMaxPageCount(inputFilePath)
+    MergePdf ([inputFilePath], pageList, None, outputFilePath)
 
 def DocToPdf (listOfDocFilePath, listOfOutputFilePath):
     """
@@ -104,6 +124,6 @@ def DocToPdf (listOfDocFilePath, listOfOutputFilePath):
         doc.SaveAs(outputDocFilePath, FileFormat=wdFormatPDF)
         doc.Close()
         word.Quit()
-
+    return
 path = r"D:\Git_Projects\SmartPdf\1.pdf"
 MergePdf ([path,path],[[3,4],[1,2,3]],["one","two"],"D:\Git_Projects\SmartPdf\out.pdf")
