ed1da6a4e47d18a9512df31bc876514cc262abc8
Shubham4792 <shubhampandey4792@gmail.com>
2018-11-03 20:51:43 -0400

    merge complete

--- gui/pdfsite/SmartPdf/views.py
+++ gui/pdfsite/SmartPdf/views.py
@@ -3,12 +3,16 @@ from .forms import FileFormset
 from .models import File
 import sys
 
-#sys.path.append(0,")
+sys.path.insert(0,'D:\Projects\Django Projects\SmartPdf\\backend')
+
+from Main import *
 # Create your views here.
 
 def index(request):
     template_name = 'index.html'
     heading_message = 'PDF Merger'
+    inputpaths = []
+    pagelists = []
     if request.method == 'GET':
         formset = FileFormset(request.GET or None)
     elif request.method == 'POST':
@@ -21,10 +25,14 @@ def index(request):
                 # extract name from each form and save
                 name = form.cleaned_data.get('name')
                 pagenos = form.cleaned_data.get('pages')
+                inputpaths.append(name)
+                maxPageCount = getMaxPageCount(name)
+                pagelists.append(expandPages(pagenos,maxPageCount))
                 # save file instance
                 if name and pagenos:
                     File(name=name,pages = pagenos).save()
                 
+            MergePdf(inputpaths,pagelists,None,r"D:\Downloads\abc.pdf")
     return render(request, template_name, {
         'formset': formset,
         'heading': heading_message,
